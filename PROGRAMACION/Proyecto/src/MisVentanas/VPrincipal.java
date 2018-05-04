aff6d3f4de108f07a784e5b9557aa8e38bfeaca5
Unknown <aitoraldava@gmail.com>
2018-05-04 12:13:26 +0200

    Cosas que faltan actualizado

--- PROGRAMACION/Proyecto/src/MisVentanas/VPrincipal.java
+++ PROGRAMACION/Proyecto/src/MisVentanas/VPrincipal.java
@@ -257,7 +257,7 @@ public class VPrincipal extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
-        // TODO add your handling code here:
+        
     }//GEN-LAST:event_jMenuItem2ActionPerformed
 
     private void cbmJNuevoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cbmJNuevoActionPerformed
